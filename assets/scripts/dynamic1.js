9572345b2da3511fc4fb21b414f30f030ad7c8fe
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-07-21 19:03:13 -0600

    new

---
+++ assets/scripts/dynamic1.js
@@ -0,0 +1,46 @@
+let parse
+
+fetch('../../index.html').then(function(response) {
+    /* when the page is loaded convert it to plain text */
+    return response.text()
+}).then(function(html) {
+    /* initialize the DOM parser */
+    var initParser = new DOMParser();
+    /* parse the text */
+     parse = initParser.parseFromString(html, 'text/html');
+    /* you can now even select part of that html as you would in the regular DOM */ 
+    /* example */
+
+}).catch(function(error) {  
+    console.log('Error fetch page: ', error);  
+});
+
+const prettyBytes = (num, precision = 3, addSpace = true) => {
+    const UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    if (Math.abs(num) < 1) return num + (addSpace ? ' ' : '') + UNITS[0];
+    const exponent = Math.min(
+      Math.floor(Math.log10(num < 0 ? -num : num) / 3),
+      UNITS.length - 1
+    );
+    const n = Number(
+      ((num < 0 ? -num : num) / 1000 ** exponent).toPrecision(precision)
+    );
+    return (num < 0 ? '-' : '') + n + (addSpace ? ' ' : '') + UNITS[exponent];
+  };
+
+const file = new File([parse], "temp.html", {
+    type: "text/html",
+  });
+  
+let size = prettyBytes(file.size)
+
+let visits
+
+fetch('https://featherapi.dani-lionn.repl.co/UpdateVisits')
+
+fetch('https://featherapi.dani-lionn.repl.co/GetVisits').then(function(res) {
+  visits = res.text()
+})
+
+document.getElementsByClassName("size").innerHTML = `The size of this HTML file is ${size}!`;
+document.getElementsByClassName("visits").innerHTML = `This website was visited ${visits} times!`;
