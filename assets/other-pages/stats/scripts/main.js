8a1f7398a454a69b6053e54ae8a4902b6d3d641f
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2023-07-28 18:01:02 -0600

    Update main.js

--- assets/other-pages/stats/scripts/main.js
+++ assets/other-pages/stats/scripts/main.js
@@ -1,3 +1,3 @@
-fetch("https://games.roblox.com/v2/users/89521500/games?accessFilter=2&limit=50&sortOrder=Asc").then(function (res) {
-  console.log(res)
+fetch('https://featherapi.dani-lionn.repl.co/GetMyGameStats').then(function (res) {
+  console.log(res.json())
 })
