495c6ed9366ab58d95f1a784df308539c1dfe14c
Dani <107291896+DaniLionn@users.noreply.github.com>
2024-03-03 14:54:37 -0700

    Update get-time.js

--- assets/pride-island-website/get-time.js
+++ assets/pride-island-website/get-time.js
@@ -1,11 +1,14 @@
+console.log("hi")
 fetch("https://play-pride-island.glitch.me/returnLastUpdated", {
   method: "GET",
 })
   .then((response) => response.json())
   .then((data) => {
+    console.log(data)
     const element = document.getElementById("date");
     const date = new Date(data.time);
     const localTime = date.toLocaleString();
+    console.log(localTIme)
 
     element.textContent = "Last updated: " + localTime;
   });
