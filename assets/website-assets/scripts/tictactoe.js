43f084335a1c9ee85df1cd5099fd4e987e55bfaa
Dani <107291896+DaniLionn@users.noreply.github.com>
2024-06-15 19:19:13 -0600

    g

---
+++ assets/website-assets/scripts/tictactoe.js
@@ -0,0 +1,14 @@
+const doc = document;
+
+const frame = doc.createElement("div");
+
+const b1 = doc.createElement("button");
+const b2 = doc.createElement("button");
+const b3 = doc.createElement("button");
+const b4 = doc.createElement("button");
+const b5 = doc.createElement("button");
+const b6 = doc.createElement("button");
+const b7 = doc.createElement("button");
+const b8 = doc.createElement("button");
+const b9 = doc.createElement("button");
+//i'll do this later
